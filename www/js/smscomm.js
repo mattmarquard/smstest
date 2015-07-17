362a7fb1e5c3aaaafab9acf3329e28436cd80733
mattmarquard <matt@thevertigo.com>
2015-07-17 12:29:01 -0700

    small update

--- www/js/smscomm.js
+++ www/js/smscomm.js
@@ -1,21 +1,29 @@
+// INPUT:
+// string = string of entire message to be processed on server
+// OUTPUT:
+// list of message partitioned so it can be sent over SMS
 function partitionMessage(string) {
+    var msgLength = string.length
     var messageComponents = [];
     // if size fits into one sms
-    if (string.length <= 80) {
-	finalHeader['seq'] = 0;
-	finalHeader['teq'] = 1;
-    	finalHeader['hash'] = hashString;
-	headAndBody['header'] = finalHeader;
-	headAndBody['body'] = string;
-	messageComponents.push(headAndBody);
+    if (msgLength <= 80) {
+	messageComponents.push(string);
     } 
     // message has to be split amongst multiple SMS
     else {
-	//TODO
+	messageComponents.push(string.slice(-80);
+	var newLength = msgLength - 80;
+	while (newLength > 0) {
+
+	var tempString = string;	
     }
     return messageComponents;
 }
 
+// INPUT:
+// models = list of backbone models to be altered
+// actions = actions to take for models provided to method
+// OUTPUT: a list of messages to be sent via SMS
 function prepareSms(models, actions) {
 
     var preString = {}
@@ -35,6 +43,7 @@ function prepareSms(models, actions) {
 
     var finalHeader = {};
     var headers = {};
+    var finalMessages;
     
     // models and actions stringfied
     var string = JSON.stringify(preString);
@@ -42,6 +51,19 @@ function prepareSms(models, actions) {
     var hashString = hash.toString(CryptoJS.enc.Hex);
     // cutting up message
     var messageComponents = partitionMessage(string);
+    var numMessages = messageComponents.length;
+    // if only one message setup finalheader with hash and return
+    if (numMessages == 1) {
+	finalHeader['seq'] = 0;
+	finalHeader['teq'] = 1;
+    	finalHeader['hash'] = hashString;
+	headAndBody['header'] = finalHeader;
+	headAndBody['body'] = string;
+	finalMessages.push(headAndBody);
+	return finalMessages;
+    }
+    for (i = 0; i <= numMessages; i ++) {
+
 
     // HEADER STUFF =============================================================================
     var header = {}
